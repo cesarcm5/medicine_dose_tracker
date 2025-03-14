2c679900a88d6bc5341c2ba54b5c5b6ae5d0a585
Cesar Contreras <cesar.alberto2003@outlook.com>
2025-03-14 02:46:49 +0000

    First layout

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,18 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../../styles/home.css";
+import pill from "../../../../assets/medicine.png"
 
 export const Navbar = () => {
 	return (
 		<nav id="navbar" className="navbar">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
+			<div className="flex">
+				<div className="flex">
+					<div>
+						<img id="pill" src={pill}/>
+					</div>
+					<div className="pt-7">
+						<p id="name">PillCheck</p>
+					</div>
 				</div>
 			</div>
 		</nav>
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -7,20 +7,15 @@ export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div id="back" className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
+		<div id="back" className="">
+			<div className="flex">
+				<p>Add Medication</p>
+				<div id="add_button" className="ml-10">
+					<button id="add_medication" className="">
+						<p id="plus" className="text-white">+</p>
+					</button>
+				</div>
 			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
 		</div>
 	);
 };
